edae24e20f6e7191f2df2dc875c1f582c6c0776a
1945437805 <1945437805@qq.com>
2021-03-06 07:56:05 +0800

    Initial commit

---
+++ babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/DependInfo.cmake
@@ -0,0 +1,85 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "G:/Clion/xcj/babyxc/startup/startup_stm32f429xx.s" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/startup/startup_stm32f429xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F429xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "G:/Clion/xcj/babyxc/Core/Src/dma2d.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Core/Src/dma2d.c.obj"
+  "G:/Clion/xcj/babyxc/Core/Src/fmc.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Core/Src/fmc.c.obj"
+  "G:/Clion/xcj/babyxc/Core/Src/gpio.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Core/Src/gpio.c.obj"
+  "G:/Clion/xcj/babyxc/Core/Src/i2c.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Core/Src/i2c.c.obj"
+  "G:/Clion/xcj/babyxc/Core/Src/ltdc.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Core/Src/ltdc.c.obj"
+  "G:/Clion/xcj/babyxc/Core/Src/main.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Core/Src/main.c.obj"
+  "G:/Clion/xcj/babyxc/Core/Src/stm32f4xx_hal_msp.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "G:/Clion/xcj/babyxc/Core/Src/stm32f4xx_it.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "G:/Clion/xcj/babyxc/Core/Src/syscalls.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Core/Src/syscalls.c.obj"
+  "G:/Clion/xcj/babyxc/Core/Src/system_stm32f4xx.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "G:/Clion/xcj/babyxc/Core/Src/usart.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Core/Src/usart.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma2d.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma2d.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dsi.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dsi.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_ltdc.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_ltdc.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_ltdc_ex.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_ltdc_ex.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sdram.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sdram.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "G:/Clion/xcj/babyxc/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_fmc.c" "G:/Clion/xcj/babyxc/cmake-build-debug/CMakeFiles/babyxc.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_fmc.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F429xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
